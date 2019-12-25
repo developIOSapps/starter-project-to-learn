99f14032be49a4be99cb63727d312112866e0ef1
Steven Hertz <hertzsteven@icloud.com>
2019-12-25 10:17:59 -0500

    Initial Commit

---
+++ Learn Activity Indicator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learn Activity Indicator
+//
+//  Created by Steven Hertz on 12/25/19.
+//  Copyright © 2019 DevelopItSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
